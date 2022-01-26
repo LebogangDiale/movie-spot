494349ac57c4cdcd0e38d9ec63f9d2ea12c85df7
Lebogang Diale <lebogang.diale@dadevs.co.za>
2022-01-26 09:49:30 +0200

    Made changes for heroku

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error('Url is Not found');
+    const error = new Error('It is Not found');
     error.status = 404;
     next(error);
 })
