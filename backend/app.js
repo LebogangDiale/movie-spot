41eef90bc2f47164aaf42e4827b66ccd27f2a74b
Lebogang Diale <lebogang.diale@dadevs.co.za>
2022-01-26 11:02:17 +0200

    Changed message

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error('Oops Kamogelo is not allowed to use this app!!!!');
+    const error = new Error('Url is not found');
     error.status = 404;
     next(error);
 })
