2c9f6f1426ab860ff6955d91185711003602db15
Lebogang Diale <lebogang.diale@dadevs.co.za>
2022-02-02 08:31:31 +0200

    Changed server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
-const serverIP = '0.0.0.0'
-const port = process.env.PORT || 3500;
+//const serverIP = '0.0.0.0'
+const port = process.env.PORT || 3000;
 
 console.log("Server running on port", port);
 
-app.listen(port, serverIP);
+app.listen(port);
